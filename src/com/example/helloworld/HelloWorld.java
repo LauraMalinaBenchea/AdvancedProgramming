75d0fb1ec0af38422d59c2a201534198a3f1ae47
LauraMalinaBenchea <75676144+LauraMalinaBenchea@users.noreply.github.com>
2021-02-16 16:53:18 +0200

    Update HelloWorld.java

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -4,12 +4,16 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //creez vectorul languages
         String[] languages = {"C", "C++", "C#", "Phyton", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        //aleg o variabila la intamplare
         int n = (int) (Math.random() * 1_000_000);
+        //efectuez operatiile cerute
         n = n * 3;
         n = n + Integer.parseInt("10101", 2);
         n = n + Integer.parseInt("FF", 16);
         n = n * 6;
+        //calculez sumele cerute
         while(n >= 10){
             int sum = 0, variable = n;
             while(variable > 0){
@@ -18,6 +22,7 @@ public class HelloWorld {
             }
             n = sum;
         }
+        //afisam textul
         System.out.print("Willy-nilly, this semester I will learn ");
         System.out.print(languages[n]);
     }
